370c37689d6c71a9846ceae7f180edfc3259ae55
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-27 14:47:57 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,14 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count,
   attr_reader :title
-  
   GENRES = []  
+  def genre=(book_genre)
+    @genre = book_genre
+    GENRES.push(book_genre)
+  end
+  def genre
+    @genre
+  end
     
   def initialize(title)
     @title = title
